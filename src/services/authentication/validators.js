5a1654a653a3dc966223adbb1b6afa9f90acaf0b
Said Furkan Dize <furkan.dize@akinon.com>
2018-12-31 22:37:38 +0300

    master [ADDED] firstname and lastname added to auth/validators

--- src/services/authentication/validators.js
+++ src/services/authentication/validators.js
@@ -5,7 +5,9 @@ export default {
   login: {
     body: {
       username: Joi.string().required(),
-      password: Joi.string().required()
+      password: Joi.string().required(),
+      firstname: Joi.string().required(),
+      lastname: Joi.string().required()
     }
   }
 };
